e64519c662b98f833f4923ea6e31f09618cb2079
Asep Jumadi <43208840+asepjumadi@users.noreply.github.com>
2026-03-18 16:27:16 +0700

    update input add new variant of text inputs

--- app/components/atomics/Input.tsx
+++ app/components/atomics/Input.tsx
@@ -4,6 +4,7 @@ type InputProps = {
   placeholder?: string;
   type?: string;
   className?: string;
+  variant?: 'light' | 'dark';
   value?: string;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
   name?: string;
@@ -13,10 +14,16 @@ export default function Input({
   placeholder = '',
   type = 'text',
   className = '',
+  variant = 'light',
   value,
   onChange,
   name,
 }: InputProps) {
+  const colorClasses =
+    variant === 'dark'
+      ? 'bg-black text-white placeholder:text-gray-400 focus:border-lime-400 focus:ring-2 focus:ring-lime-600'
+      : 'bg-white text-gray-900 placeholder:text-gray-400 focus:border-lime-500 focus:ring-2 focus:ring-lime-200';
+
   return (
     <input
       type={type}
@@ -24,7 +31,7 @@ export default function Input({
       placeholder={placeholder}
       value={value}
       onChange={onChange}
-      className={`w-full px-6 py-4 rounded-full border-2 border-gray-800 bg-white text-gray-900 placeholder:text-gray-400 focus:outline-none focus:border-lime-500 focus:ring-2 focus:ring-lime-200 transition-all duration-300 ${className}`}
+      className={`w-full px-6 py-4 rounded-full border-2 border-gray-800 ${colorClasses} focus:outline-none transition-all duration-300 ${className}`}
     />
   );
 }
